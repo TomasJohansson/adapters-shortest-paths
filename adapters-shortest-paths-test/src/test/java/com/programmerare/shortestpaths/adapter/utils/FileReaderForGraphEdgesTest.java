043c452c0c3199618f21bedd764241e6e772683f
TomasJohansson <github-tomas@programmerare.com>
2017-11-14 19:59:45 +0100

    added a class which can read string representations for edges from a textfile and create Edge instances from them. a test class and a small test file has also been added.

---
+++ adapters-shortest-paths-test/src/test/java/com/programmerare/shortestpaths/adapter/utils/FileReaderForGraphEdgesTest.java
@@ -0,0 +1,52 @@
+package com.programmerare.shortestpaths.adapter.utils;
+
+import static com.programmerare.shortestpaths.adapter.impl.WeightImpl.SMALL_DELTA_VALUE_FOR_WEIGHT_COMPARISONS;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import com.programmerare.shortestpaths.adapter.Edge;
+
+/**
+ * @author Tomas Johansson
+ */
+public class FileReaderForGraphEdgesTest {
+
+	@Test
+	public void testReadEdgesFromFile() {
+		final String filePath = "directory_for_filereader_test/two_edges_A_B_7__X_Y_8.txt";
+		// /src/test/resources/directory_for_filereader_test/file_for_filereader_test.txt
+		// the above line has two rows like below:
+		//A B 7
+		//X Y 8
+		
+		final FileReaderForGraphEdges fileReaderForGraphTestData = FileReaderForGraphEdges.createFileReaderForGraphEdges();
+		final List<Edge> edges = fileReaderForGraphTestData.readEdgesFromFile(filePath);
+		assertNotNull(edges);
+		assertEquals(2, edges.size());
+		
+		final Edge edge1 = edges.get(0); // A B 7
+		final Edge edge2 = edges.get(1); // X Y 8
+		assertNonNulls(edge1);
+		assertNonNulls(edge2);
+
+		assertEquals("A", edge1.getStartVertex().getVertexId());
+		assertEquals("B", edge1.getEndVertex().getVertexId());
+		assertEquals(7, edge1.getEdgeWeight().getWeightValue(), SMALL_DELTA_VALUE_FOR_WEIGHT_COMPARISONS);
+		
+		assertEquals("X", edge2.getStartVertex().getVertexId());
+		assertEquals("Y", edge2.getEndVertex().getVertexId());
+		assertEquals(8, edge2.getEdgeWeight().getWeightValue(), SMALL_DELTA_VALUE_FOR_WEIGHT_COMPARISONS);		
+		
+	}
+
+	private void assertNonNulls(Edge edge) {
+		assertNotNull(edge);
+		assertNotNull(edge.getStartVertex());
+		assertNotNull(edge.getEndVertex());
+		assertNotNull(edge.getEdgeWeight());
+	}
+}
