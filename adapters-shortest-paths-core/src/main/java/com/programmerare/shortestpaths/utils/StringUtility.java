ce970b38c22d9f8600f4b0866c2e1f936a359884
TomasJohansson <github-tomas@programmerare.com>
2017-11-15 00:27:19 +0100

    added a StringUtility class with a method (and test) for splitting multilined string into a list of strings while ignoring empty lines

---
+++ adapters-shortest-paths-core/src/main/java/com/programmerare/shortestpaths/utils/StringUtility.java
@@ -0,0 +1,17 @@
+package com.programmerare.shortestpaths.utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public final class StringUtility {
+	public static List<String> getMultilineStringAsListOfStringsIgnoringThoseWithWhiteSpaceOnly(String s) {
+		final String lines[] = s.split("[\r\n]+");
+		final List<String> listOfLines = new ArrayList<String>(); 
+		for (String line : lines) {
+			if(line != null && !line.trim().equals("") ) {
+				listOfLines.add(line);	
+			}
+		}
+		return listOfLines;	
+	}
+}
