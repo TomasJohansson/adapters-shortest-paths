6e8a3e1114296a733018a3ec58bdd84fd0d8f3a5
TomasJohansson <github-tomas@programmerare.com>
2017-12-18 22:58:27 +0100

    some refactoring of an adapter implementation class

--- adapters-shortest-paths-impl-reneargento/src/main/java/com/programmerare/shortestpaths/adapter/reneargento/generics/PathFinderReneArgentoGenerics.java
+++ adapters-shortest-paths-impl-reneargento/src/main/java/com/programmerare/shortestpaths/adapter/reneargento/generics/PathFinderReneArgentoGenerics.java
@@ -19,8 +19,6 @@ import chapter4.section4.EdgeWeightedDigraph;
 import chapter4.section4.KShortestPaths;
 import chapter4.section4.KShortestPaths.Path;
 
-
-
 /**
  * "Adapter" implementation of the "Target" interface 
  * @author Tomas Johansson
@@ -36,10 +34,9 @@ public class PathFinderReneArgentoGenerics
 	extends PathFinderBase<P,  E, V, W> 
 	implements PathFinderGenerics<P, E, V, W>
 {
-	KShortestPaths kShortestPaths = new KShortestPaths();	
-//	private final edu.asu.emit.algorithm.graph.Graph graphAdaptee;
+	private final KShortestPaths kShortestPaths;	
 	private final MapperForIntegerIdsAndGeneralStringIds idMapper;
-	private EdgeWeightedDigraph edgeWeightedDigraph;
+	private final EdgeWeightedDigraph edgeWeightedDigraph;
 	
 	protected PathFinderReneArgentoGenerics(
 		final GraphGenerics<E, V, W> graph 
@@ -55,48 +52,25 @@ public class PathFinderReneArgentoGenerics
 	) {
 		super(graph, pathFactory);
 
+		kShortestPaths = new KShortestPaths();
 		edgeWeightedDigraph = new EdgeWeightedDigraph(graph.getVertices().size());
-		final MapperForIntegerIdsAndGeneralStringIds idMapper = MapperForIntegerIdsAndGeneralStringIds.createIdMapper(0);
-		createListOfVerticesWhileAlsoPopulatingIdMapper(idMapper);
 		
-		// "Adaptee" https://en.wikipedia.org/wiki/Adapter_pattern		
-//		this.graphAdaptee = new GraphPossibleToCreateProgrammatically(
-//			idMapper.getNumberOfVertices(),
-//			vertices
-//		);
-		
-		
-		this.idMapper = idMapper;
+		idMapper = MapperForIntegerIdsAndGeneralStringIds.createIdMapper(0);
+		addEdgesToGraphWhileAlsoPopulatingIdMapper();
 	}
-	
-	private void createListOfVerticesWhileAlsoPopulatingIdMapper(final MapperForIntegerIdsAndGeneralStringIds idMapper) {
-		
-//		edgeWeightedDigraph.addEdge(new DirectedEdge(0, 1, 1));
-//		edgeWeightedDigraph.addEdge(new DirectedEdge(0, 2, 7));
-//		edgeWeightedDigraph.addEdge(new DirectedEdge(1, 2, 1));
-//		edgeWeightedDigraph.addEdge(new DirectedEdge(2, 1, 1));
-//		edgeWeightedDigraph.addEdge(new DirectedEdge(1, 3, 3));
-//		edgeWeightedDigraph.addEdge(new DirectedEdge(1, 4, 2));
-//		edgeWeightedDigraph.addEdge(new DirectedEdge(2, 4, 4));
-//		edgeWeightedDigraph.addEdge(new DirectedEdge(3, 4, 1));
-		
+
+	/**
+	 * Precondition: The two fields idMapper and edgeWeightedDigraph must have been created before invoking this method.
+	 */
+	private void addEdgesToGraphWhileAlsoPopulatingIdMapper() {
 		final List<E> edges = this.getGraph().getEdges();
-//		final List<EdgeYanQi> vertices = new ArrayList<EdgeYanQi>();
 		for (final E edge : edges) {
 			final int integerIdForStartVertex = idMapper.createOrRetrieveIntegerId(edge.getStartVertex().getVertexId());
 			final int integerIdForEndVertex = idMapper.createOrRetrieveIntegerId(edge.getEndVertex().getVertexId());
-//			vertices.add(new EdgeYanQi(integerIdForStartVertex, integerIdForEndVertex, edge.getEdgeWeight().getWeightValue()));
 			edgeWeightedDigraph.addEdge(new DirectedEdge(integerIdForStartVertex, integerIdForEndVertex, edge.getEdgeWeight().getWeightValue()));
 		}
 	}
 
-	/**
-	 * Note that currently there seem to be no way for the "yanqi" implementation to actually specify the number 
-	 * of shortest paths to return, but it seems to find all paths.
-	 * However, the implementation of this method instead takes care of reducing the result.
-	 * Otherwise, if the semantic of the method is not respected it can not for example be tested 
-	 * against results from other implementations since then they would return a different number of paths.      
-	 */
 	@Override
 	protected List<P> findShortestPathHook(
 		final V startVertex, 
@@ -106,12 +80,11 @@ public class PathFinderReneArgentoGenerics
 		final List<P> paths = new ArrayList<P>();
 		final int startVertexId = idMapper.createOrRetrieveIntegerId(startVertex.getVertexId());
 		final int endVertexId = idMapper.createOrRetrieveIntegerId(endVertex.getVertexId());
-		final KShortestPaths kShortestPaths = new KShortestPaths();
-		List<Path> kShortestPaths2 = kShortestPaths.getKShortestPaths(edgeWeightedDigraph, startVertexId, endVertexId, maxNumberOfPaths);
-		for (Path path : kShortestPaths2) {
+		final List<Path> shortestPaths = kShortestPaths.getKShortestPaths(edgeWeightedDigraph, startVertexId, endVertexId, maxNumberOfPaths);
+		for (final Path path : shortestPaths) {
 			final List<E> edges = new ArrayList<E>();
-			final Iterable<DirectedEdge> path2 = path.getPath();
-			for (DirectedEdge directedEdge : path2) {
+			final Iterable<DirectedEdge> edgesInPath = path.getPath();
+			for (final DirectedEdge directedEdge : edgesInPath) {
 				final E edge = getOriginalEdgeInstance(directedEdge.from(), directedEdge.to()); 
 				edges.add(
 					edge
@@ -119,14 +92,9 @@ public class PathFinderReneArgentoGenerics
 			}
 			final W totalWeight = super.createInstanceWithTotalWeight(path.weight(), edges);			
 			paths.add(super.createPath(totalWeight, edges));
-			if(maxNumberOfPaths == paths.size()) {
-				break;
-			}
 		}
 		return Collections.unmodifiableList(paths);
 	}
-
-
 
 	private E getOriginalEdgeInstance(final int startVertexForEdge, final int endVertexForEdge) {
 		final String startVertexId = idMapper.getBackThePreviouslyStoredGeneralStringIdForInteger(startVertexForEdge);
