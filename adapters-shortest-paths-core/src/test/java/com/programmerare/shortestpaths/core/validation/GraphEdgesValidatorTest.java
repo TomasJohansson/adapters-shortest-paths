cd579f3f0025fb8a534014cfaa42afb776f8aa72
TomasJohansson <github-tomas@programmerare.com>
2017-12-24 22:42:13 +0100

    added license information to the top of the java files in the core project

--- adapters-shortest-paths-core/src/test/java/com/programmerare/shortestpaths/core/validation/GraphEdgesValidatorTest.java
+++ adapters-shortest-paths-core/src/test/java/com/programmerare/shortestpaths/core/validation/GraphEdgesValidatorTest.java
@@ -1,3 +1,8 @@
+/*
+* Copyright (c) Tomas Johansson , http://www.programmerare.com
+* The code is made available under the terms of the MIT License.
+* https://github.com/TomasJohansson/adapters-shortest-paths/blob/master/adapters-shortest-paths-core/License.txt
+*/
 package com.programmerare.shortestpaths.core.validation;
 
 import static com.programmerare.shortestpaths.core.impl.generics.PathGenericsImpl.createPathGenerics;
