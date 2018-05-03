40ef3fd3dbe891356e23aa1977e84790761eb6d3
Tomas Johansson <github-tomas@programmerare.com>
2018-05-04 00:01:57 +0200

    added a convenient "test" class to example project. it is not a real test class with assertions but it is convenient to trigger code execution with maven command "mvn test" compared to specifying a main class to execute at the command prompt

---
+++ adapters-shortest-paths-example-project/src/test/java/shortest_paths_getting_started_example/ExampleMainTest.java
@@ -0,0 +1,20 @@
+package shortest_paths_getting_started_example;
+
+import org.junit.Test;
+
+/**
+ * This is not a real test class which is doing any assertions, but it is a convenient 
+ * way of running the code to write "mvn test" instead of writing (finding and pasting) 
+ * the following command:
+ * mvn exec:java -Dexec.mainClass="shortest_paths_getting_started_example.ExampleMain"
+ * 
+ * @author Tomas Johansson
+ */
+public class ExampleMainTest {
+
+	@Test
+	public void testMain() {
+		ExampleMain exampleMain = new ExampleMain();
+		exampleMain.main(null);
+	}
+}
